90b7157fd53adbc32550545ac46440fc358a88c6
mateusdegobi <kim3dis@gmail.com>
2022-07-22 22:57:45 -0300

    refactor: eliminated unnecessary component, checking and hooks.

--- src/components/Switcher/index.tsx
+++ src/components/Switcher/index.tsx
@@ -1,48 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { View } from "react-native";
 import Text from "~components/Text";
 
-import { Container, OptionButton, TextButton } from "./styled";
+import { Container, OptionButton, TextButton } from "./style";
 
 interface SwitcherProps {
   title: string;
   data: string[];
-  onSelected: React.Dispatch<React.SetStateAction<string>>;
+  value: string;
+  onChange: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Option = (props) => {
   return (
     <OptionButton onPress={props?.onPress} marked={props?.isMarked}>
-      <TextButton marked={props?.isMarked}>{props.children}</TextButton>
+      <TextButton
+        marked={props?.isMarked}
+        fontWeight="extraBold"
+        fontSize="regular"
+      >
+        {props.children}
+      </TextButton>
     </OptionButton>
   );
 };
 
-const Switcher = ({ title, data, onSelected }: SwitcherProps) => {
-  const [marked, setMarked] = useState("");
-
-  useEffect(() => onSelected(marked), [marked]);
-
-  const Items = () => {
-    if (!data || data.length === 0) return;
-
-    return data.map(item => {
-      return (
-        <Option key={item} isMarked={item === marked} onPress={() => setMarked(item)}>
-          {item}
-        </Option>
-      );
-    });
-  };
-
+const Switcher = ({ title, data, onChange, value }: SwitcherProps) => {
   return (
-    <>
+    <View style={{ marginTop: 15 }}>
       <Text fontWeight="bold" fontSize="large">
         {title}
       </Text>
       <Container>
-        <Items />
+        {data.map((item) => {
+          return (
+            <Option
+              key={item}
+              isMarked={item === value}
+              onPress={() => onChange(item)}
+            >
+              {item}
+            </Option>
+          );
+        })}
       </Container>
-    </>
+    </View>
   );
 };
 
